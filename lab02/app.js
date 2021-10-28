4f025f0c6173dfcdaa52b5d251b848c8975f58c0
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2021-10-27 18:34:36 -0700

    function for the 7th quest.Added score + attempts

--- lab02/app.js
+++ lab02/app.js
@@ -1,64 +1,127 @@
 "use strict";
+let score = 0;
 
-var numbers = [95, 98, 17, 57, 86, 72, 46, 30, 71749379, 13, 7, 16, 10];
+function guessingGame() {
+  alert(
+    "Here is a list of cars!" + "mazda",
+    "bmw",
+    "subaru",
+    "porsche",
+    "honda",
+    "lexus",
+    "toyota",
+    "chrysler",
+    "buick",
+    "Hyundai"
+  );
+  var cars = [
+    "mazda",
+    "bmw",
+    "subaru",
+    "porsche",
+    "honda",
+    "lexus",
+    "toyota",
+    "chrysler",
+    "buick",
+    "Hyundai",
+  ];
+
+  for (let i = 0; i < 10; i++) {
+    var carAnswer = prompt("Now can you guess my car??");
+
+    for (let j = 0; j < cars.length; j++) {
+      if (carAnswer === cars[j]) {
+        alert("Correct!");
+        score++;
+        i = 3;
+        break;
+        console.log(carAnswer);
+      }
+    }
+    if (i !== 3) {
+      alert("Incorrect");
+      console.log(carAnswer);
+    }
+
+    alert(`Thank you for your time ${greeting}. Please come back again!`);
+  }
+  alert(`${greeting} yo got a ${score}`);
+}
 
 document.getElementById("test").addEventListener("click", function () {
-  // let greeting;
-  // while (!greeting) {
-  //   greeting = greerompt("What is your name?");
-  //   if (greeting === null) {
-  //     alert("Not there? Bye!");
-  //     break;
-  //   }
-  //   let name = greeting.toUpperCase();
-  //   alert("Well Hello " + name);
-  //   console.log("Greeting" + name);
+  let greeting;
+  while (!greeting) {
+    greeting = prompt("What is your name?");
+    if (greeting === null) {
+      alert("Not there? Bye!");
+      break;
+    }
+    let name = greeting.toUpperCase();
+    alert("Well Hello " + name);
+    console.log("Greeting" + name);
 
-  //   var who = prompt(
-  //     "Is Mark Zuckerberg a American media magnate, internet entrepreneur, and philanthropist who co-founded Facebook?"
-  //   );
-  //   if (who === "Yes" || who === "y") {
-  //     alert("Yes Facebook is life!");
-  //   } else {
-  //     alert("No you are wrong facebook is life!");
-  //   }
-  //   var where = prompt("Did Mr. Mark Duckerberg attend Harvard?");
-  //   where.toLowerCase();
-  //   if (where === "yes" || where === "y") {
-  //     alert("Yes!, Harvard! Facebook! Facebook is life!!");
-  //   } else {
-  //     alert(`No, no, ${name} you are wrong. Facebook is still life!`);
-  //   }
+    var who = prompt(
+      "Is Mark Zuckerberg a American media magnate, internet entrepreneur, and philanthropist who co-founded Facebook?"
+    );
+    who.toLowerCase();
+    if (who === "Yes" || who === "y") {
+      alert("Yes Facebook is life!");
+      score++;
+    } else {
+      alert("No you are wrong facebook is life!");
+    }
+    var where = prompt("Did Mr. Mark Duckerberg attend Harvard?");
+    where.toLowerCase();
+    if (where === "yes" || where === "y") {
+      alert("Yes!, Harvard! Facebook! Facebook is life!!");
+      score++;
+    } else {
+      alert(`No, no, ${name} you are wrong. Facebook is still life!`);
+    }
 
-  //   var money = prompt("Was Mark Zuckerberg a millionaire by age 23?");
-  //   money.toLowerCase();
-  //   if (money === "yes" || money === "y") {
-  //     alert(`Yes! ${name} how briliant of a achievement!`);
-  //   } else {
-  //     alert(`No, no, ${name} you are wrong. Facebook is still life!`);
-  //   }
+    var money = prompt("Was Mark Zuckerberg a millionaire by age 23?");
+    money.toLowerCase();
+    if (money === "yes" || money === "y") {
+      alert(`Yes! ${name} how briliant of a achievement!`);
+      score++;
+    } else {
+      alert(`No, no, ${name} you are wrong. Facebook is still life!`);
+    }
 
-  //   var forbes = prompt("Has Mr.Zuckerberg appeared in Forbes Magazine?");
-  //   forbes.toLowerCase();
-  //   if (forbes === "yes" || forbes === "y") {
-  //     alert(
-  //       `Good job, ${name}! Another great achievement for our sentient creation!`
-  //     );
-  //   } else {
-  //     alert(`No, no, ${name} you are wrong. Facebook is still life!`);
-  //   }
-  //   alert(`Thank you for your time ${name}. Please come back again!`);
-  // }
-  let attempts = 0;
-  var kids = prompt("How many children does Mark Zuckerberg have?");
-  while (attempts < 4) {
-    kids();
-    attempts++;
+    var forbes = prompt("Has Mr.Zuckerberg appeared in Forbes Magazine?");
+    forbes.toLowerCase();
+    if (forbes === "yes" || forbes === "y") {
+      alert(
+        `Good job, ${name}! Another great achievement for our sentient creation!`
+      );
+      score++;
+    } else {
+      alert(`No, no, ${name} you are wrong. Facebook is still life!`);
+    }
   }
-  var guessingGame = prompt("Which number is my favorite?");
-  var numList = [];
-  numbers.forEach(function (obj) {
-    numList.push(obj.numbers);
-  });
-  alert(a);
+  let attempts = 0;
+
+  while (attempts > 4) {
+    var kids = prompt("How many children does Mark Zuckerberg have?");
+    if (kids === 2) {
+      alert("Correct!");
+      console.log(kids);
+      attempts++;
+    } else if (kids < 2) {
+      alert("Too many");
+      attempts++;
+      console.log(kids);
+    } else if (kids >= 1) {
+      alert("Too little");
+      attempts++;
+      console.log(kids);
+    }
+  }
+
+  score++;
+  console.log(score);
+  {
+    guessingGame();
+  }
 });
