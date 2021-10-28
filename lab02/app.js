6173e7acbaf3773631060f84a5ee0ef21dd4ef89
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2021-10-27 19:33:36 -0700

    wrapped the button JS in a function

--- lab02/app.js
+++ lab02/app.js
@@ -49,79 +49,83 @@ function guessingGame() {
   alert(`${greeting} yo got a ${score}`);
 }
 
-document.getElementById("test").addEventListener("click", function () {
-  let greeting;
-  while (!greeting) {
-    greeting = prompt("What is your name?");
-    if (greeting === null) {
-      alert("Not there? Bye!");
-      break;
-    }
-    let name = greeting.toUpperCase();
-    alert("Well Hello " + name);
-    console.log("Greeting" + name);
+function gameStart() {
+  document.getElementById("test").addEventListener("click", function () {
+    let greeting;
+    while (!greeting) {
+      greeting = prompt("What is your name?");
+      if (greeting === null) {
+        alert("Not there? Bye!");
+        break;
+      }
+      let name = greeting.toUpperCase();
+      alert("Well Hello " + name);
+      console.log("Greeting" + name);
 
-    var who = prompt(
-      "Is Mark Zuckerberg a American media magnate, internet entrepreneur, and philanthropist who co-founded Facebook?"
-    );
-    who.toLowerCase();
-    if (who === "Yes" || who === "y") {
-      alert("Yes Facebook is life!");
-      score++;
-    } else {
-      alert("No you are wrong facebook is life!");
-    }
-    var where = prompt("Did Mr. Mark Duckerberg attend Harvard?");
-    where.toLowerCase();
-    if (where === "yes" || where === "y") {
-      alert("Yes!, Harvard! Facebook! Facebook is life!!");
-      score++;
-    } else {
-      alert(`No, no, ${name} you are wrong. Facebook is still life!`);
-    }
-
-    var money = prompt("Was Mark Zuckerberg a millionaire by age 23?");
-    money.toLowerCase();
-    if (money === "yes" || money === "y") {
-      alert(`Yes! ${name} how briliant of a achievement!`);
-      score++;
-    } else {
-      alert(`No, no, ${name} you are wrong. Facebook is still life!`);
-    }
-
-    var forbes = prompt("Has Mr.Zuckerberg appeared in Forbes Magazine?");
-    forbes.toLowerCase();
-    if (forbes === "yes" || forbes === "y") {
-      alert(
-        `Good job, ${name}! Another great achievement for our sentient creation!`
+      var who = prompt(
+        "Is Mark Zuckerberg a American media magnate, internet entrepreneur, and philanthropist who co-founded Facebook?"
       );
-      score++;
-    } else {
-      alert(`No, no, ${name} you are wrong. Facebook is still life!`);
-    }
-  }
-  let attempts = 0;
+      who.toLowerCase();
+      if (who === "Yes" || who === "y") {
+        alert("Yes Facebook is life!");
+        score++;
+      } else {
+        alert("No you are wrong facebook is life!");
+      }
+      var where = prompt("Did Mr. Mark Duckerberg attend Harvard?");
+      where.toLowerCase();
+      if (where === "yes" || where === "y") {
+        alert("Yes!, Harvard! Facebook! Facebook is life!!");
+        score++;
+      } else {
+        alert(`No, no, ${name} you are wrong. Facebook is still life!`);
+      }
 
-  while (attempts > 4) {
-    var kids = prompt("How many children does Mark Zuckerberg have?");
-    if (kids === 2) {
-      alert("Correct!");
-      console.log(kids);
-      attempts++;
-    } else if (kids < 2) {
-      alert("Too many");
-      attempts++;
-      console.log(kids);
-    } else if (kids >= 1) {
-      alert("Too little");
-      attempts++;
-      console.log(kids);
-    }
-  }
+      var money = prompt("Was Mark Zuckerberg a millionaire by age 23?");
+      money.toLowerCase();
+      if (money === "yes" || money === "y") {
+        alert(`Yes! ${name} how briliant of a achievement!`);
+        score++;
+      } else {
+        alert(`No, no, ${name} you are wrong. Facebook is still life!`);
+      }
 
-  score++;
-  console.log(score);
-  {
-    guessingGame();
-  }
-});
+      var forbes = prompt("Has Mr.Zuckerberg appeared in Forbes Magazine?");
+      forbes.toLowerCase();
+      if (forbes === "yes" || forbes === "y") {
+        alert(
+          `Good job, ${name}! Another great achievement for our sentient creation!`
+        );
+        score++;
+      } else {
+        alert(`No, no, ${name} you are wrong. Facebook is still life!`);
+      }
+    }
+    let attempts = 0;
+
+    while (attempts > 4) {
+      var kids = prompt("How many children does Mark Zuckerberg have?");
+      if (kids === 2) {
+        alert("Correct!");
+        console.log(kids);
+        attempts++;
+      } else if (kids < 2) {
+        alert("Too many");
+        attempts++;
+        console.log(kids);
+      } else if (kids >= 1) {
+        alert("Too little");
+        attempts++;
+        console.log(kids);
+      }
+    }
+
+    score++;
+    console.log(score);
+    {
+      guessingGame();
+    }
+  });
+}
+
+gameStart();
